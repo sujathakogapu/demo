9948cb8461b173ebf254a0c524207df768af3648
sujathakogapu <ksuji142@gmail.com>
2023-04-16 11:50:28 +0530

    NumberTriangle

---
+++ src/com/example/basics/NumberTriAnglePattern.java
@@ -0,0 +1,19 @@
+package com.example.basics;
+
+import java.util.Scanner;
+
+public class NumberTriAnglePattern {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a no of rows:");
+		int row = scanner.nextInt();
+		for (int i = 1; i <= row; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+		}
+		scanner.close();
+	}
+
+}
