d5471540acbc17836cc24a36fdd2630d5d30113c
sujathakogapu <ksuji142@gmail.com>
2023-05-05 11:45:25 +0530

    Code Refactoring

--- src/com/example/patterns/Diamond.java
+++ src/com/example/patterns/Diamond.java
@@ -1,28 +1,40 @@
 package com.example.patterns;
+/*
+5.Print a diamond pattern of stars using nested loops.
+  Input:
+  Enter the number of rows: 5
+  Output:
+  *
+ ***
+*****
+ ***
+  *
+*/
+import java.util.Scanner;
 
 public class Diamond {
 	public static void main(String[] args) {
-		{
-			int number = 7;
-			int m, n;
-			for (m = 1; m <= number; m++) {
-				for (n = 1; n <= number - m; n++) {
-					System.out.print(" ");
-				}
-				for (n = 1; n <= m * 2 - 1; n++) {
-					System.out.print("*");
-				}
-				System.out.println();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Entera no of rows:");
+		int row = scanner.nextInt();
+		scanner.close();
+		for (int i = 1; i <= row; i++) {//1,2,3,4,5
+			for (int j = 1; j <= row - i; j++) {//1234,123,12,1
+				System.out.print(" ");
 			}
-			for (m = number - 1; m > 0; m--) {
-				for (n = 1; n <= number - m; n++) {
-					System.out.print(" ");
-				}
-				for (n = 1; n <= m * 2 - 1; n++) {
-					System.out.print("*");
-				}
-				System.out.println();
+			for (int j = 1; j <= i * 2 - 1; i++) {//1,3,5,7,9
+				System.out.print("*");
 			}
+			System.out.println();
+		}
+		for (int i = row - 1; i > 0; i--) {//4321,321,21,1
+			for (int j = 1; j <= row - i; j++) {//1234,123,12,1
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i * 2 - 1; j++) {//1,3,5,7,9
+				System.out.print("*");
+			}
+			System.out.println();
 		}
 	}
 }
