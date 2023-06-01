76edf83320fac961a2ca4edb0709770c89a8441b
sujathakogapu <ksuji142@gmail.com>
2023-06-01 12:43:49 +0530

    Exception program

---
+++ src/com/example/exceptions/Test.java
@@ -0,0 +1,16 @@
+package com.example.exceptions;
+
+public class Test {
+	public static void main(String[] args) {
+		Object name="sujatha";
+		Object salary=12000;
+		Object[] arr=new Object[5];
+		
+		String nameStr=(String) name;
+		int sal=(int) salary;
+		
+		String empSalary="20000";
+		int empSal=Integer.parseInt(empSalary);
+	}
+
+}
