4d8df412edcb5c109d9a21b0401e1e0202600ecb
sujathakogapu <ksuji142@gmail.com>
2023-06-21 10:54:42 +0530

    steps add to programs

--- src/com/example/collections/FrequencyOfEachCharacter.java
+++ src/com/example/collections/FrequencyOfEachCharacter.java
@@ -23,26 +23,38 @@ import java.util.LinkedHashMap;
 import java.util.Scanner;
 
 public class FrequencyOfEachCharacter {
+	//method creation 
 		public LinkedHashMap<Character, Integer> charCountFrequency(String Str,LinkedHashMap<Character, Integer>charFreqMap) {
+			//logic for frequency chars
+			//loop for take length given string
 			for (int i = 0; i < Str.length(); i++) {
+				//each char assign in ch variable
 				Character ch = Str.charAt(i);
+				//charfreqmap contains ch then count is increment
 				if (charFreqMap.containsKey(ch)) {
 					int count = charFreqMap.get(ch);
 					charFreqMap.put(ch, count + 1);
+					//else count is put in ch
 				} else {
 					charFreqMap.put(ch, 1);
 				}
 			}
+			//return charFreqMap
 			return charFreqMap;
 		}
 
 		public static void main(String[] args) {
+			//take inputs from user
 			Scanner sc = new Scanner(System.in);
 			System.out.println("Enter a string:");
 			String Str = sc.nextLine();
+			// LinkedHashMap object creation
 			LinkedHashMap<Character,Integer> charFreqMap=new LinkedHashMap<>();
+			//main class object creation
 			FrequencyOfEachCharacter Obj = new FrequencyOfEachCharacter();
+			//method calling
 			LinkedHashMap<Character, Integer> Output = Obj.charCountFrequency(Str,charFreqMap);
+			//output print
 			System.out.println("Character frequency:"+ charFreqMap);
 			sc.close();
 		}
--- src/com/example/collections/FrequencyOfWords.java
+++ src/com/example/collections/FrequencyOfWords.java
@@ -20,27 +20,39 @@ Word frequency:
 
  */
 public class FrequencyOfWords {
+	//method creation
 	public LinkedHashMap<String,Integer> getWordsFrequency(String str){
+		//linkedHashMap object creation with genirics
 		LinkedHashMap<String,Integer> linkedHashMap=new LinkedHashMap<>();
+		//split the words for given string using split method and assign in String array
 		String[] words=str.split(" ");
+		//create for each loop count the words
 		for(String word:words) {
+			//if contains word then count is increment and put in linkedHashmap object
 			if(linkedHashMap.containsKey(word)) {
 				int count=linkedHashMap.get(word);
 				count++;
 				linkedHashMap.put(word,count);
 			}
+			//else count is constant
 			else
 				linkedHashMap.put(word,1);
 		}
+		//return thr map
 		return linkedHashMap;
 	}
 	public static void main(String[] args) {
+		//take input from user
 		Scanner scanner=new Scanner(System.in);
+		//LinkedHashMap object creation
 		LinkedHashMap<String,Integer> map=new LinkedHashMap<>();
 		System.out.println("Enter a sentence: ");
 		String str=scanner.nextLine();
+		//main class object creation
 		FrequencyOfWords obj=new FrequencyOfWords();
+		//method calling
 		LinkedHashMap<String,Integer> lmap=obj.getWordsFrequency(str);
+		//print output
 		System.out.println("Word frequency: "+lmap);
 	}
 		}
