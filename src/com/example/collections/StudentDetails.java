2648e465533ef193e9281dc48e843543bf31cad7
sujathakogapu <ksuji142@gmail.com>
2023-06-23 19:42:14 +0530

    Code Refactoring

--- src/com/example/collections/StudentDetails.java
+++ src/com/example/collections/StudentDetails.java
@@ -3,6 +3,7 @@ package com.example.collections;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 /*Assignment 2: Create a Student class with instance variables - studentId(String) and name(String). 
@@ -15,51 +16,71 @@ public class StudentDetails {
 	private String studentId;
 	private String studentName;
 	private double studentGPA;
-	public StudentDetails(String studentId,String studentName,double studentGPA) {
-		this.studentId=studentId;
-		this.studentName=studentName;
-		this.studentGPA=studentGPA;
+
+	public StudentDetails(String studentId, String studentName, double studentGPA) {
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.studentGPA = studentGPA;
 	}
+
 	@Override
-		public boolean equals(Object obj) {
-		if(this==obj) {
+	public boolean equals(Object obj) {
+		if (this == obj) {
 			return true;
 		}
-		StudentDetails student=(StudentDetails)obj;
-		if(this.equals(student.studentId)&&this.studentName.equals(student.studentName)&& this.studentGPA==(student.studentGPA)) {
+		StudentDetails student = (StudentDetails) obj;
+		if (this.equals(student.studentId) && this.studentName.equals(student.studentName)
+				&& this.studentGPA == (student.studentGPA)) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
-		}
+	}
+
 	@Override
-		public int hashCode() {
-		int result=this.studentName.hashCode();
-		result=result+this.studentId.hashCode();
+	public int hashCode() {
+		int result = this.studentName.hashCode();
+		result = result + this.studentId.hashCode();
 		return result;
-		}
-	
+	}
 
 	@Override
 	public String toString() {
 		return "StudentDetails [studentId=" + studentId + ", studentName=" + studentName + ", studentGPA=" + studentGPA
 				+ "]";
 	}
-	public static void main(String[] args) {
-		Scanner scanner=new Scanner(System.in);
-		System.out.println("Enter no of students gpas: ");
-		int size=scanner.nextInt();
-		ArrayList<Double> students=new ArrayList<>();
-		System.out.println("Enter student gpas: ");
-		double gpas=scanner.nextDouble();
-		for(int i=0;i<size;i++) {
-			students.add(scanner.nextDouble());
+	
+	public int compareTo(StudentDetails obj) {
+		if (this.studentGPA < obj.studentGPA) {
+			return -1;
+		} else if (this.studentGPA > obj.studentGPA) {
+			return 1;
+
+		} else {
+			return 0;
 		}
-		System.out.println("Before collection sort students gpas: " +students);
-		Collections.sort(students);
-		System.out.println("After collection sort students gpas: "+students);
+
+	}
+
+
+	public static void main(String[] args) {
+		ArrayList<StudentDetails> students=new ArrayList<>();
+		StudentDetails student1=new StudentDetails("12345","sujatha",  10.55);
+		StudentDetails student2=new StudentDetails("123456","Shiva",  5.55);
+		int compare=student1.compareTo(student2);
+		if(compare==1) {
+			System.out.println(student1.studentName + " GPA is greater than "+ student2.studentName+" GPA");
+		}else if(compare==-1){
+			System.out.println(student1.studentName + " GPA is less than "+ student2.studentName+" GPA");
+		}
+		else {
+			System.out.println(student1.studentName + " and  "+ student2.studentName+ " have the same GPA");
+		}
+		students.add(student1);
+		students.add(student2);
+		System.out.println("Before collection sort students gpas: " + students);
+		//Collections.sort( (List<T>) students);
+		System.out.println("After collection sort students gpas: " + students);
 	}
 
 }
-
-
