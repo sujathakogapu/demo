3ad98255b8de512eaa279fb23d33ae5341b6366a
sujathakogapu <ksuji142@gmail.com>
2023-06-23 12:48:15 +0530

    code Refactoring

--- src/com/example/collections/Employee.java
+++ src/com/example/collections/Employee.java
@@ -11,18 +11,16 @@ Two employee objects are equal if they have same employeeId and name.
   In the Employee class add an instance variable salary(float) and implement Comparable interface to compare salaries. 
   Create a Arraylist of employees and sort them using Collections.sort() method.
 */
-public class Employee implements Comparable<Employee>{
+public class Employee implements Comparable<Employee> {
 	private String employeeId;
 	private String name;
 	private float salary;
-	
 
-	public Employee(String employeeId, String name,float salary) {
+	public Employee(String employeeId, String name, float salary) {
 		this.employeeId = employeeId;
 		this.name = name;
-		this.salary=salary;
+		this.salary = salary;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -30,7 +28,7 @@ public class Employee implements Comparable<Employee>{
 			return true;
 		}
 		Employee emp = (Employee) obj;
-		if (this.employeeId.equals(emp.employeeId) && this.name.equals(emp.name) && this.salary==(emp.salary)) {
+		if (this.employeeId.equals(emp.employeeId) && this.name.equals(emp.name) && this.salary == (emp.salary)) {
 			return true;
 		} else {
 			return false;
@@ -44,29 +42,45 @@ public class Employee implements Comparable<Employee>{
 		return result;
 	}
 
+	
 	@Override
 	public String toString() {
-		return "Employee [employeeId=" + employeeId + ", name=" + name + "]";
-	}
-	public static void main(String[] args) {
-		Scanner scanner=new Scanner(System.in);
-		System.out.println("Enter no of employee salaries: ");
-		int size=scanner.nextInt();
-		ArrayList<Float> employee=new ArrayList<>();
-		System.out.println("Enter employee salaries: ");
-		float salary=scanner.nextFloat();
-		for(int i=0;i<size;i++) {
-			employee.add(scanner.nextFloat());
-		}
-		System.out.println("Before collection sort employee salaries: " +employee);
-		Collections.sort(employee);
-		System.out.println("After collection sort employee salaries: "+employee);
+		return "Employee [employeeId=" + employeeId + ", name=" + name + ", salary=" + salary + "]";
 	}
 
 	@Override
-	public int compareTo(Employee o) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int compareTo(Employee obj) {
+		if (this.salary < obj.salary) {
+			return -1;
+		} else if (this.salary > obj.salary) {
+			return 1;
+
+		} else {
+			return 0;
+		}
+
 	}
+
+	public static void main(String[] args) {
+		ArrayList<Employee> employee=new ArrayList<>();
+		Employee emp1=new Employee("12345","sujatha", (float) 10.5);
+		Employee emp2=new Employee("123456","Shiva", (float) 5.5);
+		int compare=emp1.compareTo(emp2);
+		if(compare==1) {
+			System.out.println(emp1.name + " salary is greater than "+ emp2.name+" salary");
+		}else if(compare==-1){
+			System.out.println(emp1.name + " salary is less than "+ emp2.name+" salary");
+		}
+		else {
+			System.out.println(emp1.name + " and  "+ emp2.name+ " have the same salary");
+		}
+		employee.add(emp1);
+		employee.add(emp2);
+		System.out.println("Before collection sort employee salaries: " + employee);
+		Collections.sort(employee);
+		System.out.println("After collection sort employee salaries: " + employee);
+	}
+
+	
 
 }
